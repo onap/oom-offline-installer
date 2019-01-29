29b11869ae403e02a408071ba4ecda295a674d39
Tomáš Levora <t.levora@partner.samsung.com>
2019-01-29 14:25:04 +0100

    Create offline data lists to cover Casablanca
    Adding casablanca related data lists and changing the lists naming rule
    
    Modifying related download scripts accordingly
    
    Adding parameters for project version to download scripts to be able to
    use version specific lists
    
    Removing download-oom.sh as it should be done in helm charts
    preparation and patching steps
    
    Moving additional build related stuff to build directory
    
    Adding missing tags
    
    Issue-ID: OOM-1615
    
    Change-Id: Ic5902f646a1f0f40ba615562f8d9b1a43cf8480b
    Signed-off-by: Tomáš Levora <t.levora@partner.samsung.com>

--- build/download_offline_data_by_lists.sh
+++ build/download_offline_data_by_lists.sh
@@ -22,27 +22,37 @@
 # fail fast
 set -e
 
+usage () {
+    echo "Usage:"
+    echo -e "./$(basename $0) <project version>\n"
+    echo "Example:"
+    echo "  ./$(basename $0) onap_3.0.0"
+}
+
 # boilerplate
 RELATIVE_PATH=./ # relative path from this script to 'common-functions.sh'
 if [ "$IS_COMMON_FUNCTIONS_SOURCED" != YES ] ; then
     SCRIPT_DIR=$(dirname "${0}")
     LOCAL_PATH=$(readlink -f "$SCRIPT_DIR")
     . "${LOCAL_PATH}"/"${RELATIVE_PATH}"/common-functions.sh
+
+if [ "${1}" == "-h" ] || [ -z "${1}" ]; then
+    usage
+    exit 0
+else
+    TAG="${1}"
 fi
 
 CTOOLS="${LOCAL_PATH}/creating_data"
-LISTS_DIR="${LOCAL_PATH}/data_list"
-DATA_DIR="${LOCAL_PATH}/../resources"
-TOTAL=12
+LISTS_DIR="${LOCAL_PATH}/data_lists"
+DATA_DIR="${LOCAL_PATH}/../../resources"
+TOTAL=10
 CURR=1
 
 message info "Downloading started: $(date)"
 
 echo "[Step $((CURR++))/$TOTAL Download collected docker images]"
-$CTOOLS/download-docker-images.sh "$LISTS_DIR/docker_image_list.txt"
-
-echo "[Step $((CURR++))/$TOTAL Download manually collected docker images]"
-$CTOOLS/download-docker-images.sh "$LISTS_DIR/docker_manual_image_list.txt"
+$CTOOLS/download-docker-images.sh "$LISTS_DIR/${TAG}-docker_images.list"
 
 echo "[Step $((CURR++))/$TOTAL Build own nginx image]"
 $CTOOLS/create_nginx_image/01create-image.sh
@@ -56,25 +66,22 @@ mv "$DATA_DIR/offline_data/docker_images_for_nexus/own_nginx_latest.tar" "$DATA_
 mv "$DATA_DIR/offline_data/docker_images_for_nexus/sonatype_nexus3_latest.tar" "$DATA_DIR/offline_data/docker_images_infra"
 
 echo "[Step $((CURR++))/$TOTAL Download git repos]"
-$CTOOLS/download-git-repos.sh "$LISTS_DIR" "$DATA_DIR/git-repo"
+$CTOOLS/download-git-repos.sh "${TAG}" "$DATA_DIR/git-repo"
 
 echo "[Step $((CURR++))/$TOTAL Download http files]"
-$CTOOLS/download-http-files.sh "$LISTS_DIR/http_manual_list.txt" "$DATA_DIR/http"
+$CTOOLS/download-http-files.sh "$LISTS_DIR/http_files.list" "$DATA_DIR/http"
 
 echo "[Step $((CURR++))/$TOTAL Download npm pkgs]"
-$CTOOLS/download-npm-pkgs.sh "$LISTS_DIR/npm_list.txt" "$DATA_DIR/offline_data/npm_tar"
+$CTOOLS/download-npm-pkgs.sh "$LISTS_DIR/${TAG}-npm.list" "$DATA_DIR/offline_data/npm_tar"
 
 echo "[Step $((CURR++))/$TOTAL Download bin tools]"
-$CTOOLS/download-bin-tools.sh "$DATA_DIR/downloads"
+$CTOOLS/download-bin-tools.sh "${TAG}" "$DATA_DIR/downloads"
 
 echo "[Step $((CURR++))/$TOTAL Download rhel pkgs]"
 $CTOOLS/download-pkg.sh "$DATA_DIR/pkg/rhel"
 
-echo "[Step $((CURR++))/$TOTAL Download oom]"
-$CTOOLS/download-oom.sh "$DATA_DIR" "${LOCAL_PATH}/../patches/offline-changes.patch"
-
 echo "[Step $((CURR++))/$TOTAL Download sdnc-ansible-server packages]"
-$CTOOLS/download-pip.sh "$LISTS_DIR/pip_list.txt" "$DATA_DIR/pkg/ubuntu/ansible_pkg"
-$CTOOLS/download-files.sh "$LISTS_DIR/pkg_list.txt" "$DATA_DIR/pkg/ubuntu/ansible_pkg"
+$CTOOLS/download-pip.sh "$LISTS_DIR/${TAG}-pip_list.txt" "$DATA_DIR/offline_data/pypi"
+$CTOOLS/download-files.sh "$LISTS_DIR/pkg_list.txt" "$DATA_DIR/pkg/ubuntu/xenial"
 
 message info "Downloading finished: $(date)"
