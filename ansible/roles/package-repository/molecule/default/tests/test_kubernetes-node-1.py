885694bcc02161673c8779004122de8ab452101a
Samuli Silvius <s.silvius@partner.samsung.com>
2019-03-22 15:37:39 +0200

    Molecule tests for package-repository role
    Issue-ID: OOM-1755
    
    Change-Id: Iac20072200f24a3102c9ed2edf5c5aa8054dd9c2
    Signed-off-by: Samuli Silvius <s.silvius@partner.samsung.com>

---
+++ ansible/roles/package-repository/molecule/default/tests/test_kubernetes-node-1.py
@@ -0,0 +1,16 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('kubernetes-node-1')
+
+
+def test_onap_repo(host):
+    fc = host.file('/etc/yum.repos.d/moleculetestapp.repo').content_string
+    expected_content = """[moleculetestapp]
+baseurl = http://repo.infra-server/rhel
+enabled = 1
+gpgcheck = 0
+name = MOLECULETESTAPP offline repository"""
+    assert fc == expected_content
