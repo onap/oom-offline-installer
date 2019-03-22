3be94e442b03dbf4710eafef1a5079a120ca384d
Samuli Silvius <s.silvius@partner.samsung.com>
2019-03-22 10:00:43 +0200

    Building and Dockerfile for centos7
    Add initial Dockerfile for centos7 for molecule testing.
    
    Issue-ID: OOM-1750
    
    Change-Id: Ie4da3d80c713d1da56465c598774b370b4c01b36
    Signed-off-by: Samuli Silvius <s.silvius@partner.samsung.com>

---
+++ ansible/test/images/docker/build-all.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+#   COPYRIGHT NOTICE STARTS HERE
+
+#   Copyright 2019 © Samsung Electronics Co., Ltd.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#   COPYRIGHT NOTICE ENDS HERE
+
+BUILD_SCRIPT=${1:-build.sh}
+
+# Run all build scripts in direct subdirectories
+for buildfile in $(find -mindepth 2 -maxdepth 2 -name ${BUILD_SCRIPT});
+do
+  pushd $(dirname ${buildfile})
+  . ${BUILD_SCRIPT}
+  popd
+done
