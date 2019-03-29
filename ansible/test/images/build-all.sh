f78661a1f4921062f2a8b505b21e9d5c2b6f14a3
Samuli Silvius <s.silvius@partner.samsung.com>
2019-03-29 13:32:35 +0200

    Molecule test run wrappers against CI
    Add ci-molecule.sh script to work as an interface
    towards CI pipe for running Molecule tests.
    
    Issue-ID: OOM-1749
    
    Change-Id: Ic684975d43aad878c89f54aa95a22b39a60301af
    Signed-off-by: Samuli Silvius <s.silvius@partner.samsung.com>

---
+++ ansible/test/images/build-all.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+#   COPYRIGHT NOTICE STARTS HERE
+
+#   Copyright 2019 © Samsung Electronics Co., Ltd.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#   COPYRIGHT NOTICE ENDS HERE
+
+#
+# Build all pre-build Molecule platform images
+#
+
+SCRIPT_DIR=$(dirname "${0}")
+LOCAL_PATH=$(readlink -f "$SCRIPT_DIR")
+
+${LOCAL_PATH}/docker/centos7/build.sh
+
