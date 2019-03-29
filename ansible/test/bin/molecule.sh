747a8e202cf5f959206ad1af2bf38f7ea1a6440f
Samuli Silvius <s.silvius@partner.samsung.com>
2019-03-29 13:20:28 +0200

    Molecule container for running Molecule
    Add own Dockerfile and build script for creating
    customized molecule runner image from Molecule image
    provided by https://quay.io/repository/ansible/molecule.
    
    Customization enables running of molecule container with
    current host's user to avoid root owned files bloating
    to developer's machine.
    
    Add wrapper script molecule.sh to utilize custom image
    and suitable for testing offline-installer roles.
    
    Issue-ID: OOM-1768
    
    Change-Id: I14ed75decb8fa637c044675543de56f1c0796271
    Signed-off-by: Samuli Silvius <s.silvius@partner.samsung.com>

---
+++ ansible/test/bin/molecule.sh
@@ -0,0 +1,54 @@
+#! /usr/bin/env bash
+
+#   COPYRIGHT NOTICE STARTS HERE
+
+#   Copyright 2019 © Samsung Electronics Co., Ltd.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#   COPYRIGHT NOTICE ENDS HERE
+
+SCRIPT_DIR=$(dirname "${0}")
+LOCAL_PATH=$(readlink -f "$SCRIPT_DIR")
+# main root dir in this git repo (relative to this script location)
+ROOT_DIR=$(cd ${LOCAL_PATH}/../../../ && pwd)/
+PROJECT_ROOT_IN_CONTAINER=${PWD##${ROOT_DIR}}
+# Make dir structure same in container to make localhost ssh actions to match same dirs
+CONTAINER_ROOT=${ROOT_DIR}
+
+if [ "${PWD}" == "${PROJECT_ROOT_IN_CONTAINER}" ]; then
+    echo "Please run it under subdir of ${ROOT_DIR} directory"
+    exit 1
+fi
+
+env_params=(--env LOCALHOST_ANSIBLE_HOST=$(hostname -I | cut -f 1 -d ' '))
+env_params+=(--env LOCALHOST_ANSIBLE_USER=$(id -nu))
+
+if [ -e "${LOCAL_PATH}/.env" ]; then
+    env_file="--env-file ${LOCAL_PATH}/.env"
+fi
+
+MOLECULE_IMAGE=${MOLECULE_IMAGE:-molecule-dev}
+MOLECULE_IMAGE_VERSION=${MOLECULE_IMAGE_VERSION:-2.20.0}
+echo "Running molecule image: ${MOLECULE_IMAGE}:${MOLECULE_IMAGE_VERSION}"
+MOLECULE_CMD=${MOLECULE_CMD:-molecule}
+docker run --rm -it \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ${ROOT_DIR}:${CONTAINER_ROOT}:rw \
+    -w ${CONTAINER_ROOT}/${PROJECT_ROOT_IN_CONTAINER} \
+    "${env_params[@]}" \
+    ${env_file} \
+    --name ${MOLECULE_IMAGE} \
+    ${MOLECULE_IMAGE}:${MOLECULE_IMAGE_VERSION} \
+    ${MOLECULE_CMD} "$@"
+
