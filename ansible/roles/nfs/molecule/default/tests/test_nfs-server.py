1a5f6edfcb93c77c74740bbeb8406af557289800
Michal Zegan <m.zegan@samsung.com>
2019-04-09 15:21:52 +0200

    Improve nfs role
    This commit improves the nfs role by
    using cluster_ip variable instead of ansible_host for getting
    nfs client/server address, and
    using /etc/exports.d instead of /etc/exports for
    exporting nfs filesystem.
    Also, this commit fixes other
    issues in molecule found along the way.
    
    Change-Id: Ibd4ea5d4fd937116e2dc9e1b6f85c3e019f62e17
    Issue-ID: OOM-1770
    Signed-off-by: Michal Zegan <m.zegan@samsung.com>

--- ansible/roles/nfs/molecule/default/tests/test_nfs-server.py
+++ ansible/roles/nfs/molecule/default/tests/test_nfs-server.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 
+import testinfra
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -19,7 +20,9 @@ def test_svc(host, svc):
 
 
 def test_exports(host):
-    f = host.file("/etc/exports")
+    node2_ip = testinfra.get_host("docker://kubernetes-node-2").interface(
+        "eth0").addresses[0]
+    f = host.file("/etc/exports.d/dockerdata-nfs.exports")
     assert f.exists
     assert f.content_string == \
-        """/dockerdata-nfs  kubernetes-node-2(rw,sync,no_root_squash,no_subtree_check)"""  # noqa: E501
+        """/dockerdata-nfs  """ + node2_ip + """(rw,sync,no_root_squash,no_subtree_check)"""  # noqa: E501
