66be0727ec3f106678c6182c9ef5ee2ea7c1c646
Samuli Silvius <s.silvius@partner.samsung.com>
2019-04-17 19:42:59 +0300

    Molecule tests for application role.
    Issue-ID: OOM-1812
    
    Change-Id: Ifb6f5a10afb4014b20be77a4141371e561d346ce
    Signed-off-by: Samuli Silvius <s.silvius@partner.samsung.com>

---
+++ ansible/roles/application/molecule/default/tests/test_default.py
@@ -0,0 +1,29 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_helm_commands(host):
+    fc = host.file('/tmp/helm_simu_output').content_string
+    expected_content = """home
+init --upgrade --skip-refresh
+version --tiller-connection-timeout 10
+repo list
+serve
+repo list
+repo add local http://127.0.0.1:8879
+install --name moleculetestapp local/moleculetestapp --namespace \
+moleculetestapp -f /opt/moleculetestapp/override.yaml"""
+    assert fc == expected_content
+
+
+def test_helm_override_file(host):
+    fc = host.file('/opt/moleculetestapp/override.yaml').content_string
+    expected_content = """global:
+    cacert: 'this is dummy server certificate value
+
+        '"""
+    assert fc == expected_content
