92083c05e29d66fa7b8579b4a1941019cd2f6225
Mateusz Pilat <m.pilat@partner.samsung.com>
2019-04-17 12:19:10 +0200

    Add fix for Molecule Docker
    Molecule Docker was not running when built by the root user
    
    Issue-ID: OOM-1808
    Change-Id: I27c3ecb6d5670ac1c447acba688b25e10d5a5a4d
    Signed-off-by: Mateusz Pilat <m.pilat@partner.samsung.com>

--- ansible/test/molecule-docker/build.sh
+++ ansible/test/molecule-docker/build.sh
@@ -1,15 +1,3 @@
-#! /usr/bin/env bash
-
-#   COPYRIGHT NOTICE STARTS HERE
-
-#   Copyright 2019 © Samsung Electronics Co., Ltd.
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +8,7 @@
 
 SCRIPT_DIR=$(dirname "${0}")
 LOCAL_PATH=$(readlink -f "$SCRIPT_DIR")
+if [ $(id -u ${USER}) -eq "0" ]; then DOCKER_USER="root"; else DOCKER_USER="molecule"; fi
 
 set -e
 
@@ -27,6 +16,7 @@ docker build \
     --build-arg USER_ID=$(id -u ${USER}) \
     --build-arg GROUP_ID=$(id -g ${USER}) \
     --build-arg DOCKER_GROUP_ID=$(getent group docker | cut -f 3 -d ':') \
+    --build-arg DOCKER_USER=${DOCKER_USER} \
     -t molecule-dev:2.20.0 \
     ${LOCAL_PATH}
 
