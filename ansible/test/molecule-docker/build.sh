988375b3b607a3177821aed8b4092987a66046ff
Mateusz Pilat <m.pilat@partner.samsung.com>
2019-04-17 12:19:10 +0200

    Add fix for Molecule Docker
    Molecule Docker was not running when built by the root user
    
    Issue-ID: OOM-1808
    Change-Id: I27c3ecb6d5670ac1c447acba688b25e10d5a5a4d
    Signed-off-by: Mateusz Pilat <m.pilat@partner.samsung.com>

--- ansible/test/molecule-docker/build.sh
+++ ansible/test/molecule-docker/build.sh
@@ -2,7 +2,7 @@
 
 #   COPYRIGHT NOTICE STARTS HERE
 
-#   Copyright 2019 © Samsung Electronics Co., Ltd.
+#   Copyright 2019 . Samsung Electronics Co., Ltd.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@
 
 SCRIPT_DIR=$(dirname "${0}")
 LOCAL_PATH=$(readlink -f "$SCRIPT_DIR")
+if [ $(id -u ${USER}) -eq "0" ]; then DOCKER_USER="root"; else DOCKER_USER="molecule"; fi
 
 set -e
 
@@ -27,6 +28,7 @@ docker build \
     --build-arg USER_ID=$(id -u ${USER}) \
     --build-arg GROUP_ID=$(id -g ${USER}) \
     --build-arg DOCKER_GROUP_ID=$(getent group docker | cut -f 3 -d ':') \
+    --build-arg DOCKER_USER=${DOCKER_USER} \
     -t molecule-dev:2.20.0 \
     ${LOCAL_PATH}
 
