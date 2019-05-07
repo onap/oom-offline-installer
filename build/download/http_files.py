910492b57af994528f44cb6d6a1ac98dcaac3a7b
Milan Verespej <m.verespej@partner.samsung.com>
2019-05-07 14:50:20 +0200

    WIP: Improve download of http files
    Issue-ID: OOM-1803
    
    Change-Id: If2aeb1aa4fab27641511a6300832ccd7be95e846
    Signed-off-by: Milan Verespej <m.verespej@partner.samsung.com>

---
+++ build/download/http_files.py
@@ -0,0 +1,120 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#   COPYRIGHT NOTICE STARTS HERE
+
+#   Copyright 2019 © Samsung Electronics Co., Ltd.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#   COPYRIGHT NOTICE ENDS HERE
+
+
+import argparse
+import concurrent.futures
+import progressbar
+import logging
+import os
+import requests
+import sys
+from retrying import retry
+
+
+progressbar.streams.wrap_stderr()
+log = logging.getLogger(__name__)
+
+
+def load_list(data_list):
+    with open(data_list, 'r') as f:
+        return {http_file for http_file in (line.strip() for line in f) if http_file}
+
+
+@retry(stop_max_attempt_number=5, wait_fixed=2000)
+def get_file(file_uri):
+    if not file_uri.startswith('http'):
+        file_uri = 'http://' + file_uri
+    file_req = requests.get(file_uri)
+    file_req.raise_for_status()
+    return file_req.content
+
+
+def save_to_file(dst, content):
+    dst_dir = os.path.dirname(dst)
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    with open(dst, 'wb') as dst_file:
+        dst_file.write(content)
+
+
+def download_file(file_uri, output_dir):
+    log.info('Downloading: {}'.format(file_uri))
+    file_content = get_file(file_uri)
+    dst_path = '{}/{}'.format(output_dir, file_uri.rsplit('//')[-1])
+    save_to_file(dst_path, file_content)
+    return file_uri
+
+
+def download(data_list, output_dir, progress):
+    file_set = load_list(data_list)
+
+    if progress is not None:
+        progress.max_value = len(file_set)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(download_file, f, output_dir) for f in file_set]
+        for future in concurrent.futures.as_completed(futures):
+            error = future.exception()
+            if error is None:
+                log.info('Downloaded: {}'.format(future.result()))
+                if progress is not None:
+                    progress.update(progress.value + 1)
+            else:
+                log.error(error)
+
+
+def init_progress():
+    progress_widgets = ['Downloading http files: ',
+                        progressbar.Bar(), ' ',
+                        progressbar.Percentage(), ' ',
+                        '(', progressbar.SimpleProgress(), ')' , ' ',
+                        progressbar.Timer(), ' ',
+                        progressbar.ETA()]
+    progress = progressbar.ProgressBar(widgets=progress_widgets,
+                                       redirect_stdout=True).start()
+    return progress
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Download http files from list')
+    parser.add_argument('file_list', metavar='file-list',
+                        help='File with list of http files to download')
+    parser.add_argument('--output-dir', '-o', default=os.getcwd(),
+                        help='Destination directory for saving')
+
+    parser.add_argument('--debug', action='store_true', default=False,
+                        help='Turn on debug output')
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    progress = init_progress()
+
+    download(args.file_list, args.output_dir, progress)
+
+if __name__ == '__main__':
+    main()
+
