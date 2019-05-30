fd39ee1e73cff0e4152e71e7b5d24af8627e3aed
Tomáš Levora <t.levora@partner.samsung.com>
2019-05-30 09:31:24 +0200

    Remove tag parameter from docker-image-collector.sh
    Removing tag parameter from docker-image-collector.sh as it's not
    necessary as described in OOM-1881
    
    Issue-ID: OOM-1885
    
    Change-Id: Id83b396c4b994cfa659c518e3f03e704a84d65f0
    Signed-off-by: Tomáš Levora <t.levora@partner.samsung.com>

--- build/creating_data/docker-images-collector.sh
+++ build/creating_data/docker-images-collector.sh
@@ -30,9 +30,9 @@ usage () {
     echo "      "
     echo "  This script is preparing docker images list based on kubernetes project"
     echo "      Usage:"
-    echo "        ./$(basename $0) <project version> <path to project> [<output list file>]"
+    echo "        ./$(basename $0) <path to project> [<output list file>]"
     echo "      "
-    echo "      Example: ./$(basename $0) onap_3.0.2 /root/oom/kubernetes/onap"
+    echo "      Example: ./$(basename $0) /root/oom/kubernetes/onap"
     echo "      "
     echo "      Dependencies: helm, python-yaml, make"
     echo "      "
@@ -61,28 +61,28 @@ create_list() {
 }
 
 # Configuration
-TAG="${1}"
-PROJECT_DIR="${2}"
-LIST="${3}"
+if [ "${1}" == "-h" ] || [ "${1}" == "--help" ] || [ $# -lt 1 ]; then
+    usage
+fi
+
+PROJECT_DIR="${1}"
+LIST="${2}"
 LISTS_DIR="$(readlink -f $(dirname ${0}))/../data_lists"
 HELM_REPO="local http://127.0.0.1:8879"
+PROJECT="$(basename ${1})"
 
-if [ "${1}" == "-h" ] || [ "${1}" == "--help" ] || [ $# -lt 2 ]; then
-    usage
-elif [ ! -f "${PROJECT_DIR}/../Makefile" ]; then
+if [ ! -f "${PROJECT_DIR}/../Makefile" ]; then
     echo "Wrong path to project directory entered"
     exit 1
 elif [ -z "${LIST}" ]; then
     mkdir -p ${LISTS_DIR}
-    LIST="${LISTS_DIR}/${TAG}-docker_images.list"
+    LIST="${LISTS_DIR}/${PROJECT}_docker_images.list"
 fi
 
 if [ -e "${LIST}" ]; then
     mv -f "${LIST}" "${LIST}.bk"
     MSG="$(realpath ${LIST}) already existed\nCreated backup $(realpath ${LIST}).bk\n"
 fi
-
-PROJECT="$(basename ${2})"
 
 # Setup helm
 if pgrep -x "helm" > /dev/null; then
