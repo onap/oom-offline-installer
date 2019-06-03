2f97acdcc45c70ee2fa022fdeb27b68a0f7e458b
Michal Zegan <m.zegan@samsung.com>
2019-06-03 14:58:20 +0200

    Add etcd group support to rke role
    Etcd cluster is now placed in members of kubernetes-etcd group instead
    of kubernetes-node.
    Due to defaults, it means infra is the only etcd
    by default.
    
    Change-Id: Iae05a42442849e4a248d73c2d78f5e2b0eae7255
    Issue-ID: OOM-1778
    Signed-off-by: Michal Zegan <m.zegan@samsung.com>

--- ansible/roles/rke/molecule/default/tests/test_etcd.py
+++ ansible/roles/rke/molecule/default/tests/test_etcd.py
@@ -4,10 +4,10 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('kubernetes-node')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('kubernetes-etcd')
 
 
 @pytest.mark.parametrize('container_name', [
-  'kubelet', 'kube-proxy'])
+  'etcd'])
 def test_container_running(host, container_name):
     assert host.docker(container_name).is_running
