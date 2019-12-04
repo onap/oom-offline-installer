f4886d83e7f166fbd9767e300467b27ec6ca9978
Denis Kasanic <d.kasanic@partner.samsung.com>
2019-12-04 13:40:07 +0100

    Add robot config at onap install
    Change onap network cidr to /16 due to robot hardcoded config.
    Adding onap network id to configuration at installation part.
    
    Issue-ID: OOM-1996
    Signed-off-by: Denis Kasanic <d.kasanic@partner.samsung.com>
    Change-Id: I4403e8ca0f385060ac815372ffd91eda60026ddb

--- patches/update_robot.sh
+++ patches/update_robot.sh
@@ -2,11 +2,11 @@
 
 set -xe
 
-ROBOT_POD=`kubectl  get pods | grep robot | awk {'print $1'}`
+ROBOT_POD=`kubectl -n onap get pods | grep robot | awk {'print $1'}`
 ROBOT_HOME="/var/opt/ONAP"
 
 # distribute example private key onap-dev
-kubectl cp onap-dev.pem ${ROBOT_POD}:${ROBOT_HOME}/onap-dev.pem
+kubectl -n onap cp onap-dev.pem ${ROBOT_POD}:${ROBOT_HOME}/onap-dev.pem
 
 # stability improvement for SRPOL lab
 # there is an issue that cloudinit is randomly putting default route
@@ -14,4 +14,4 @@ kubectl cp onap-dev.pem ${ROBOT_POD}:${ROBOT_HOME}/onap-dev.pem
 # this patch assume that we are using rc3-offline-network as public network for vFW VMs
 # vFW VMs are installing SW in runtime, similarly as other ONAP demo usecases
 # please note that such network must be reachable from robot pod
-kubectl cp base_vfw.yaml ${ROBOT_POD}:${ROBOT_HOME}/demo/heat/vFW/base_vfw.yaml
+kubectl -n onap cp base_vfw.yaml ${ROBOT_POD}:${ROBOT_HOME}/demo/heat/vFW/base_vfw.yaml
