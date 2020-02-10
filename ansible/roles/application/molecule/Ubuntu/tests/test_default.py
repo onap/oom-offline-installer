c786e1268164de31b2d6bd757acef0e701cea9b5
Jan Benedikt <j.benedikt@partner.samsung.com>
2020-02-10 14:45:09 +0100

    Adding Ubuntu support in Ansible - application role
    Extending ansible playbooks of ubuntu support. Creating new test with Ubuntu image for Molecule in application role.
    
    Issue-ID: OOM-1671
    Change-Id: I7662506263a143cb7193583d2058766ac7829d93
    Signed-off-by: Jan Benedikt <j.benedikt@partner.samsung.com>

---
+++ ansible/roles/application/molecule/Ubuntu/tests/test_default.py
@@ -0,0 +1,31 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_helm_commands(host):
+    fc = host.file('/tmp/helm_simu_output').content_string
+    expected_content = """home
+init --upgrade --skip-refresh
+version --tiller-connection-timeout 10
+repo list
+serve
+repo list
+repo add local http://127.0.0.1:8879
+install --name moleculetestapp local/moleculetestapp --namespace \
+moleculetestapp -f /opt/moleculetestapp/helm_charts/onap/resources/\
+overrides/onap-all.yaml -f /opt/moleculetestapp/override.yaml \
+--timeout 1800"""
+    assert fc == expected_content
+
+
+def test_helm_override_file(host):
+    fc = host.file('/opt/moleculetestapp/override.yaml').content_string
+    expected_content = """global:
+    cacert: 'this is dummy server certificate value
+
+        '"""
+    assert fc == expected_content
