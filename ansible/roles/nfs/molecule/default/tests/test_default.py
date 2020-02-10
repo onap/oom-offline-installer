2619b9525d6fd3ca76dbd24e6d1bdbebdec9ffc8
Jan Benedikt <j.benedikt@partner.samsung.com>
2020-02-10 16:06:37 +0100

    Adding Ubuntu support in Ansible - nfs role
    Extending ansible playbooks of ubuntu support.
    Creating new test with Ubuntu image for Molecule in nfs role.
    
    Issue-ID: OOM-1671
    Signed-off-by: Jan Benedikt <j.benedikt@partner.samsung.com>
    Change-Id: Ib1c85e4df7ec2ba547a20684ed5e23e097351df2

--- ansible/roles/nfs/molecule/default/tests/test_default.py
+++ ansible/roles/nfs/molecule/default/tests/test_default.py
@@ -7,10 +7,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize('pkg', [
-  'nfs-utils'
+@pytest.mark.parametrize('distro,pkg', [
+  ('centos', 'nfs-utils'),
+  ('ubuntu', 'nfs-common'),
+  ('ubuntu', 'nfs-kernel-server')
 ])
-def test_pkg(host, pkg):
-    package = host.package(pkg)
-
-    assert package.is_installed
+def test_pkg(host, distro, pkg):
+    os = host.system_info.distribution
+    if distro == os:
+        package = host.package(pkg)
+        assert package.is_installed
--- ansible/roles/nfs/molecule/default/tests/test_nfs-server.py
+++ ansible/roles/nfs/molecule/default/tests/test_nfs-server.py
@@ -20,9 +20,14 @@ def test_svc(host, svc):
 
 
 def test_exports(host):
+    os = host.system_info.distribution
+    if (os == "centos"):
+        host_file = "/etc/exports.d/dockerdata-nfs.exports"
+    elif (os == "ubuntu"):
+        host_file = "/etc/exports"
     node2_ip = testinfra.get_host("docker://kubernetes-node-2").interface(
         "eth0").addresses[0]
-    f = host.file("/etc/exports.d/dockerdata-nfs.exports")
+    f = host.file(host_file)
     assert f.exists
     assert f.content_string == \
         """/dockerdata-nfs  """ + node2_ip + """(rw,sync,no_root_squash,no_subtree_check)"""  # noqa: E501
