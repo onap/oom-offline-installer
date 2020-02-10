5d62278104206139360088599a8b12a85f81f0e3
Jan Benedikt <j.benedikt@partner.samsung.com>
2020-02-10 16:29:43 +0100

    Adding Ubuntu support in Ansible - package-repository role
    Extending ansible playbooks of ubuntu support.
    Creating new test with Ubuntu image for Molecule in package-repository role.
    
    Issue-ID: OOM-1671
    Signed-off-by: Jan Benedikt <j.benedikt@partner.samsung.com>
    Change-Id: I85d4e6857536fa06c7eb4f4042c1ed64fcbf903e

---
+++ ansible/roles/package-repository/molecule/ubuntu/tests/test_kubernetes-node-1.py
@@ -0,0 +1,13 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('kubernetes-node-1')
+
+
+def test_onap_repo(host):
+    fc = host.file('/etc/apt/sources.list.d/moleculetestapp.list')
+    fc = fc.content_string
+    ec = "deb [trusted=yes] http://repo.infra-server/deb ./"
+    assert fc == ec
