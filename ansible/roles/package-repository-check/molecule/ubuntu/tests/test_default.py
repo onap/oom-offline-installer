7e2cb5d76ac6c7ff52c0de99e29d4a676d45a179
Jan Benedikt <j.benedikt@partner.samsung.com>
2020-05-05 08:46:19 +0200

    Fix lint errors in package-repository-check role
    In package-repository-check was changed command module to service module due to lint failing.
    Failing was caused by errors:
    [301] Commands should not change things if nothing needs doing
    [303] service used in place of service module
    
    Added molecule test.
    
    Issue-ID: OOM-2395
    Signed-off-by: Jan Benedikt <j.benedikt@partner.samsung.com>
    Change-Id: I6cfdfe920eabfb9a0c43cc9ba4ffcd81df1d6852

---
+++ ansible/roles/package-repository-check/molecule/ubuntu/tests/test_default.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('distro,pkg', [
+  ('ubuntu', 'resolvconf')
+])
+def test_pkg(host, distro, pkg):
+    os = host.system_info.distribution
+    if distro == os:
+        package = host.package(pkg)
+        assert package.is_installed
