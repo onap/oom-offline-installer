e2b009b1c28575e34904b9f2275ced1c4ec06fec
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-09-15 16:51:24 +0200

    Add feature to install shell autocompletion for kubectl
    Change-Id: I732dcd92144f9a8489a351047ab88b53ca36759e
    Issue-ID: OOM-2564
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

--- ansible/roles/kubectl/molecule/default/tests/test_default.py
+++ ansible/roles/kubectl/molecule/default/tests/test_default.py
@@ -9,3 +9,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_kubectl(host):
     assert host.file('/usr/local/bin/kubectl').exists
     assert host.run('kubectl').rc != 127
+
+
+def test_kubectl_bash_completion(host):
+    assert host.file('/etc/bash_completion.d/kubectl').exists
