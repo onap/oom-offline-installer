3253aef57ddfc285b61ac1df12ee9e24a0e915b9
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2021-01-26 09:35:25 +0100

    Split Helm v2 setup to separate playbook
    Logically splitting the application playbook to put
    Helm v2 and v3 specific setup actions into separate
    tasks file.
    
    Change-Id: I20446a196bb2687e27fdeb6a45a83565b676fc42
    Issue-ID: OOM-2665
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

--- ansible/roles/application/molecule/default/tests/test_default.py
+++ ansible/roles/application/molecule/default/tests/test_default.py
@@ -10,16 +10,19 @@ def test_helm_commands(host):
     fc = host.file('/tmp/helm_simu_output').content_string
     helm_release = host.ansible.get_variables()['helm_version']
     if helm_release == 'v2':
-        content_str1 = 'home'
-    elif helm_release == 'v3':
-        content_str1 = 'env'
-    expected_content = content_str1 + """
+        expected_content = """home
 init --upgrade --skip-refresh
 version --tiller-connection-timeout 10
 repo list
 serve
 repo list
 repo add local http://127.0.0.1:8879
+deploy moleculetestapp local/moleculetestapp --namespace \
+moleculetestapp -f /opt/moleculetestapp/helm_charts/onap/resources/\
+overrides/onap-all.yaml -f /opt/moleculetestapp/override.yaml \
+--timeout 1800"""
+    elif helm_release == 'v3':
+        expected_content = """env
 deploy moleculetestapp local/moleculetestapp --namespace \
 moleculetestapp -f /opt/moleculetestapp/helm_charts/onap/resources/\
 overrides/onap-all.yaml -f /opt/moleculetestapp/override.yaml \
