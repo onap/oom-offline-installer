2cb3b64a67104e0e854ad72b1c89eb0b631bc9d0
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2021-11-25 12:40:10 +0100

    [MOLECULE] Remove separate helm cases from 'application' role verifier
    Change-Id: I0a9fc3bad5ec639deabdf50f42f7798974e830bc
    Issue-ID: OOM-2883
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

--- ansible/roles/application/molecule/default/tests/test_default.py
+++ ansible/roles/application/molecule/default/tests/test_default.py
@@ -8,30 +8,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_helm_commands(host):
     fc = host.file('/tmp/helm_simu_output').content_string.strip()
-    helm_release = host.ansible.get_variables()['helm_version']
-    if helm_release == 'v2':
-        expected_content = """home
-init --upgrade --skip-refresh
-version --tiller-connection-timeout 10
-repo list
-serve
-repo list
-repo add local http://127.0.0.1:8879
-deploy moleculetestapp local/moleculetestapp --namespace \
-moleculetestapp -f /opt/moleculetestapp/helm_charts/onap/resources/\
-overrides/onap-all.yaml -f /opt/moleculetestapp/override.yaml \
---timeout 1800"""
-        expected_plugin_path = '/plugins/deploy/deploy.sh'
-    elif helm_release == 'v3':
-        expected_content = """env
+    expected_content = """env
 repo list
 repo add local http://127.0.0.1:8879
 deploy moleculetestapp local/moleculetestapp --namespace \
 moleculetestapp -f /opt/moleculetestapp/helm_charts/onap/resources/\
 overrides/onap-all.yaml -f /opt/moleculetestapp/override.yaml \
 --timeout 1800s"""
-        expected_plugin_path = '/root/.local/share/helm/plugins/deploy/' +\
-                               'deploy.sh'
+    expected_plugin_path = '/root/.local/share/helm/plugins/deploy/' +\
+                           'deploy.sh'
     assert fc == expected_content
     assert host.file(expected_plugin_path).exists
 
