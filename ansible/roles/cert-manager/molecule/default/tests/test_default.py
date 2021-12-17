c5103f9e65d92f49219e51d2c14e6ea7d3efa625
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2021-12-17 11:29:42 +0100

    [ANSIBLE] Drop shell completion code generation in 'cert-manager' role
    Moved to a dedicated role
    
    Change-Id: Ibfaf132d4d81edaee8332b809c3452d530ab1c4c
    Issue-ID: OOM-2902
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

--- ansible/roles/cert-manager/molecule/default/tests/test_default.py
+++ ansible/roles/cert-manager/molecule/default/tests/test_default.py
@@ -12,12 +12,3 @@ def test_cmctl(host):
     assert f.user == 'root'
     assert f.group == 'root'
     assert host.run('cmctl').rc == 0
-
-
-def test_bash_completion(host):
-    assert host.package("bash-completion").is_installed
-
-
-def test_bash_completion_cmctl(host):
-    f = host.file('/etc/bash_completion.d/cmctl')
-    assert f.exists
