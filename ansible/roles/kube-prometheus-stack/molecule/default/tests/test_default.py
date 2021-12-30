dc1c3afc4925afbe72502b94187ad6ee2308d7f7
Ostap Batih <o.batih@samsung.com>
2021-12-30 14:54:31 +0000

    [MOLECULE] Add testinfra tests for kube-prometheus-stack role
    Issue-ID: OOM-2889
    Signed-off-by: Ostap Batih <o.batih@samsung.com>
    Change-Id: I64af5aadcc14c864a27806f20d56e0d7007ff33d

---
+++ ansible/roles/kube-prometheus-stack/molecule/default/tests/test_default.py
@@ -0,0 +1,32 @@
+import yaml
+
+
+def test_prometheus_helm_package(host):
+    assert host.file('/opt/onap/downloads/'
+                     'kube-prometheus-stack-18.0.4.tgz').exists
+
+
+def test_helm_values_file(host):
+    y = None
+    f = host.file('/opt/onap/kube_prometheus_values.yaml')
+    assert f.exists
+    assert f.is_file
+    try:
+        y = yaml.safe_load(f.content)
+    except yaml.YAMLError:
+        assert False
+    assert y['grafana']['adminPassword'] == 'grafana'
+    assert (y['grafana']['env']['GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH'] ==
+            '/var/lib/grafana/dashboards/grafana_home.json')
+    assert (y['grafana']['persistence']['storageClassName'] ==
+            'kube-prometheus-grafana')
+    assert (y['prometheus']['prometheusSpec']['storageSpec']
+            ['volumeClaimTemplate']['spec']['storageClassName'] ==
+            'kube-prometheus-prometheus')
+
+
+def test_grafana_dashboards(host):
+    assert host.file('/nfs/kube-prometheus/kube-prometheus-grafana/'
+                     'dashboards/custom/grafana_dashboard.json').exists
+    assert host.file('/nfs/kube-prometheus/kube-prometheus-grafana/'
+                     'dashboards/grafana_home.json').exists
